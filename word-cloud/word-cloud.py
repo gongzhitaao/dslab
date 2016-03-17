eb7238a9a26527e02085bd3aca37b7bc74140e8a
Zhitao Gong <zhitaao.gong@gmail.com>
2016-03-17 14:49:24 -0500

    word cloud

---
+++ word-cloud/word-cloud.py
@@ -0,0 +1,25 @@
+import pybtex.database as pybib_db
+import wordcloud as wc
+import matplotlib.pyplot as plt
+
+bib_data = pybib_db.parse_file('weishinn-ku.bib')
+bib_data = bib_data.lower()
+
+titles = []
+for entry in bib_data.entries.values():
+    titles.append(entry.fields['title'])
+
+print("Total {0} titles.".format(len(titles)))
+
+text = ' '.join(titles)
+
+stopwords = set([x.strip()
+                 for x in open('stopwords', 'r').read().split('\n')])
+cloud_generator = wc.WordCloud(
+    width=1000, height=600, margin=5, background_color='white',
+    stopwords=stopwords)
+title_cloud = cloud_generator.generate(text)
+plt.imshow(title_cloud)
+plt.tight_layout(True)
+plt.axis('off')
+plt.show()
